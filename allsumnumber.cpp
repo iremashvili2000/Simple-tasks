959e79a5003904cfc2b6a9478269621903ad83b7
iremashvili2000 <34402636+iremashvili2000@users.noreply.github.com>
2019-10-23 20:37:17 +0400

    Create allsumnumber.cpp

---
+++ allsumnumber.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+//numbers whose sum equals the number entered by the user
+	int number1, a, b, c;
+	cout << "please enter number: ";
+	cin >> number1;
+	b = number1;
+	for (int i = 0; i <= number1/2; i++) {
+		cout << i << "+" << b << "="<<number1 << endl;
+		b--;
+	}
+	
+
+
+
+
+
+
+}
